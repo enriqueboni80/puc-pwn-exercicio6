d087db877d5462fde0d9e509ba74b29f5a1ac7b5
Enrique-Note\Enrique <enriqueboni80@gmail.com>
2019-12-02 11:26:30 -0300

    Exercicio Finalizado

--- routes/productsController.js
+++ routes/productsController.js
@@ -1,23 +1,51 @@
 let products = {
-    items: [
-        {
+    items: [{
             id: 1,
             name: 'Product 1',
             description: 'Product1 description',
             price: 19.00
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            description: 'Product2 description',
+            price: 30.00
         }
     ]
 }
 
 module.exports = {
     get(_, res) {
-        res.json({ title: 'Products page' });
+        res.json({ products: products.items });
     },
     getById(req, res) {
         if (!req.params.id) {
             res.json({ error: 'Should receive an id' })
         }
+        products.items.forEach(element => {
+            if (req.params.id == element.id) {
+                res.json({ success: element })
+            }
+        });
+    },
+    store(req, res) {
 
-        res.json({ success: 'Id received!' })
+        if (req.body.description.length < 10) {
+            res.json({ msg: 'descricao menor que 10' })
+        } else if (req.body.price <= 0) {
+            res.json({ msg: 'preco menor ou igual a zero' })
+        } else {
+            var lastArrayPosition = products.items.length - 1
+            var newElement = {
+                id: products.items[lastArrayPosition].id + 1,
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price
+            }
+            if (products.items.push(newElement)) {
+                console.log(newElement)
+                res.json({ msg: 'criado com sucesso' })
+            }
+        }
     }
 };
--- tests/routes/products.test.js
+++ tests/routes/products.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 
-const { get, getById } = require('../../routes/productsController');
+const {get, getById, store } = require('../../routes/productsController');
 
 let req = {
     body: {},
@@ -14,11 +14,27 @@ const res = {
     }
 }
 
+let products = {
+    items: [{
+            id: 1,
+            name: 'Product 1',
+            description: 'Product1 description',
+            price: 19.00
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            description: 'Product2 description',
+            price: 30.00
+        }
+    ]
+}
+
 describe('Products Route', function() {
     describe('get() function', function() {
         it('should return object with title ', function() {
             get(req, res);
-            expect(res.jsonCalledWith).to.be.eql({ title: 'Products page'});
+            expect(res.jsonCalledWith).to.be.eql({ products: products.items });
         });
 
         it('should receive return by id ', function() {
@@ -26,9 +42,40 @@ describe('Products Route', function() {
             getReq.params = {
                 id: 1
             };
-
             getById(getReq, res);
             expect(res.jsonCalledWith).to.be.have.key('success')
+        });
+    })
+
+    describe('post() function', function() {
+        it('should return created with sucess of post', function() {
+            req.body = {
+                name: 'galo',
+                description: 'galo galo galo galo',
+                price: 13.00
+            }
+            store(req, res);
+            expect(res.jsonCalledWith).to.be.eql({ msg: 'criado com sucesso' });
+        });
+
+        it('should return descricao menor que 10 caracteres', function() {
+            req.body = {
+                name: 'galo',
+                description: '123456789',
+                price: 13.00
+            }
+            store(req, res);
+            expect(res.jsonCalledWith).to.be.eql({ msg: 'descricao menor que 10' });
+        });
+
+        it('should return preco menor ou igual a zero', function() {
+            req.body = {
+                name: 'galo',
+                description: '12345678910',
+                price: 0
+            }
+            store(req, res);
+            expect(res.jsonCalledWith).to.be.eql({ msg: 'preco menor ou igual a zero' });
         });
     })
 });
